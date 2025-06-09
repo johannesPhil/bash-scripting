1fad1ee20da2a75bf0e722d59146fd1b18d477e0
John Peter <51692893+johannesPhil@users.noreply.github.com>
2025-06-09 17:43:13 +0100

    Update file_exists.sh

--- file_exists.sh
+++ file_exists.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 if [ -z "$1" ]; then
-  echo "File name not supplied. Usage: $0 <filename>"
+  echo "File not supplied. Usage: $0 <filename>"
   exit 1
 fi
 
 file_name="$1"
 
-echo "Searching for $filename..."
+echo "Searching for $file_name in current directory..."
 
-found=$(find . -type f -name "$filename")
+found=$(find . -type f -name "$file_name")
 
 if [ -n "$found" ]; then
   echo "Found!"
   echo "$found"
 
 else
-  echo "File $filename not found"
+  echo "File $file_name not found"
 fi
